2d79baddf1d0a9e0bf1742b36ec8db1574d9fc03
Aayushi-Agrwal <khushiagarwal01464@gmail.com>
2023-10-04 00:03:29 +0530

    feat: data display

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { Suspense } from "react";
 config.autoAddCss = false;
 
 const inter = Inter({ subsets: ["latin"] });
@@ -20,7 +21,9 @@ export default function RootLayout({
   config.autoAddCss = false;
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <Suspense fallback={<h1 className="text-white">Loading</h1>}>
+        <body className={inter.className}>{children}</body>
+      </Suspense>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -12,7 +12,6 @@ import {
   faCloud,
   faTemperatureHigh,
   faTemperatureLow,
-  faUmbrella,
 } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
@@ -20,19 +19,37 @@ import {
   WeatherDivForADay,
   WeatherDivForAWeek,
 } from "./Components";
-import { SetStateAction, useEffect, useState } from "react";
+import { Suspense, useEffect, useState } from "react";
 import axios from "axios";
+import Loading from "./loading";
 
 interface WeatherData {
   city: {
     name: string;
   };
-  list: [{ main: { temp: number } }];
+  list: [
+    {
+      main: {
+        temp: number;
+        feels_like: number;
+        temp_min: number;
+        temp_max: number;
+        humidity: number;
+        pressure: number;
+      };
+      wind: {
+        speed: number;
+        deg: number;
+      };
+      visibility: number;
+    }
+  ];
 }
 
 export default function Home() {
   const [location, setLocation] = useState<any>(null);
   const [weatherData, setWeatherData] = useState<WeatherData | null>(null);
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     // Get user's geolocation
@@ -48,12 +65,15 @@ export default function Home() {
         const weatherApiUrl = `https://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=afa686b6ddf43261ad3dc386077429f8`;
 
         axios
-          .get(weatherApiUrl)
+          .get<WeatherData>(weatherApiUrl)
           .then((weatherResponse) => {
             setWeatherData(weatherResponse.data);
           })
           .catch((error) => {
             console.error("Error fetching weather data:", error);
+          })
+          .finally(() => {
+            setLoading(false); // Set loading to false after data fetching is complete
           });
       },
       (error) => {
@@ -63,87 +83,116 @@ export default function Home() {
   }, []); // Empty dependency array ensures that this effect runs once after the initial render
 
   return (
-    <main className="flex min-h-screen items-center justify-between bg-[url(/storm-clouds.png)] bg-no-repeat bg-cover text-white">
-      <div className="border-r-2 border-x-slate-400 w-1/6 h-screen flex flex-col px-2 items-center justify-around py-12">
-        <div className="flex gap-2">
-          <h1 className="text-6xl">
-            {weatherData?.list[0].main.temp}
-            <p
-              className="text-sm"
-              onClick={() => console.log(weatherData?.list[0].main.temp)}
-            >
-              Feels like: 20
-            </p>
-          </h1>
-          <div className="py-1">
-            <div className="flex gap-2 items-center">
-              <FontAwesomeIcon icon={faTemperatureHigh} size="1x" />
-              <h3 className="">23°</h3>
+    <>
+      {loading ? (
+        <div>Loading</div>
+      ) : (
+        <main className="flex min-h-screen items-center justify-between bg-[url(/storm-clouds.png)] bg-no-repeat bg-cover text-white">
+          <div className="border-r-2 border-x-slate-400 w-1/6 h-screen flex flex-col px-2 items-center justify-around py-12">
+            <div className="flex gap-2">
+              <h1 className="text-6xl">
+                {weatherData?.list[0].main.temp &&
+                  Math.round(weatherData.list[0].main.temp - 273)}
+                °
+                <p className="text-sm">
+                  Feels like:{" "}
+                  {weatherData?.list[0].main.feels_like &&
+                    Math.round(weatherData.list[0].main.feels_like - 273)}
+                  °
+                </p>
+              </h1>
+              <div className="py-1">
+                <div className="flex gap-2 items-center">
+                  <FontAwesomeIcon icon={faTemperatureHigh} size="1x" />
+                  <h3 className="">
+                    {" "}
+                    {weatherData?.list[0].main.temp_max &&
+                      Math.round(weatherData.list[0].main.temp_max - 273)}
+                    °
+                  </h3>
+                </div>
+
+                <div className="flex gap-2 items-center">
+                  <FontAwesomeIcon icon={faTemperatureLow} size="1x" />
+                  <h3 className="">
+                    {weatherData?.list[0].main.temp_min &&
+                      Math.round(weatherData.list[0].main.temp_min - 273)}
+                    °
+                  </h3>
+                </div>
+              </div>
             </div>
 
-            <div className="flex gap-2 items-center">
-              <FontAwesomeIcon icon={faTemperatureLow} size="1x" />
-              <h3 className="">18°</h3>
+            <div className="flex flex-col gap-2">
+              <WeatherDetailsDay
+                icon={faDroplet}
+                text={`Humidity: ${weatherData?.list[0].main.humidity}%`}
+              />
+              <WeatherDetailsDay
+                icon={faWind}
+                text={`Wind: ${weatherData?.list[0].wind.speed} m/s at ${weatherData?.list[0].wind.deg}°`}
+              />
+              <WeatherDetailsDay
+                icon={faEye}
+                text={`Visibility: ${weatherData?.list[0].visibility}`}
+              />
+              <WeatherDetailsDay
+                icon={faGauge}
+                text={`Pressure: ${weatherData?.list[0].main.pressure} hPa `}
+              />
+            </div>
+
+            <div className="flex gap-8 mb-24">
+              <p className="flex gap-2 items-center">
+                <FontAwesomeIcon icon={faSun} size="1x" color="yellow" />
+                19000
+              </p>
+
+              <p className="flex gap-2 items-center">
+                <FontAwesomeIcon icon={faMoon} size="1x" color="grey" />
+                19000
+              </p>
             </div>
           </div>
-        </div>
+          <div className="w-5/6 h-screen flex flex-col justify-around">
+            <div className="px-24">
+              <p>
+                <FontAwesomeIcon icon={faMapPin} size="1x" /> Abomsa
+              </p>
+              {/* name */}
+              <h2 className="text-6xl">
+                Few Clouds <FontAwesomeIcon icon={faCloud} size="1x" />
+              </h2>
+              {/* Description */}
 
-        <div className="flex flex-col gap-2">
-          <WeatherDetailsDay icon={faDroplet} text="Humidity: 23%" />
-          <WeatherDetailsDay icon={faWind} text="Wind: 2.06 at 40°" />
-          <WeatherDetailsDay icon={faEye} text="Visibility: 6000" />
-          <WeatherDetailsDay icon={faGauge} text="Pressure: 1007" />
-        </div>
+              <p className="flex gap-2 items-center py-2 text-sm">
+                12/01/20
+                <FontAwesomeIcon icon={faClock} size="1x" />
+                19000
+              </p>
+            </div>
 
-        <div className="flex gap-8 mb-24">
-          <p className="flex gap-2 items-center">
-            <FontAwesomeIcon icon={faSun} size="1x" color="yellow" />
-            19000
-          </p>
+            <div className="flex w-full justify-around px-16">
+              <WeatherDivForADay />
+              <WeatherDivForADay />
+              <WeatherDivForADay />
+              <WeatherDivForADay />
+              <WeatherDivForADay />
+              <WeatherDivForADay />
+              <WeatherDivForADay />
+              <WeatherDivForADay />
+            </div>
 
-          <p className="flex gap-2 items-center">
-            <FontAwesomeIcon icon={faMoon} size="1x" color="grey" />
-            19000
-          </p>
-        </div>
-      </div>
-      <div className="w-5/6 h-screen flex flex-col justify-around">
-        <div className="px-24">
-          <p>
-            <FontAwesomeIcon icon={faMapPin} size="1x" /> Abomsa
-          </p>
-          {/* name */}
-          <h2 className="text-6xl">
-            Few Clouds <FontAwesomeIcon icon={faCloud} size="1x" />
-          </h2>
-          {/* Description */}
-
-          <p className="flex gap-2 items-center py-2 text-sm">
-            12/01/20
-            <FontAwesomeIcon icon={faClock} size="1x" />
-            19000
-          </p>
-        </div>
-
-        <div className="flex w-full justify-around px-16">
-          <WeatherDivForADay />
-          <WeatherDivForADay />
-          <WeatherDivForADay />
-          <WeatherDivForADay />
-          <WeatherDivForADay />
-          <WeatherDivForADay />
-          <WeatherDivForADay />
-          <WeatherDivForADay />
-        </div>
-
-        <div className="flex justify-around border-t-2 border-slate-400 pt-8">
-          <WeatherDivForAWeek />
-          <WeatherDivForAWeek />
-          <WeatherDivForAWeek />
-          <WeatherDivForAWeek />
-          <WeatherDivForAWeek />
-        </div>
-      </div>
-    </main>
+            <div className="flex justify-around border-t-2 border-slate-400 pt-8">
+              <WeatherDivForAWeek />
+              <WeatherDivForAWeek />
+              <WeatherDivForAWeek />
+              <WeatherDivForAWeek />
+              <WeatherDivForAWeek />
+            </div>
+          </div>
+        </main>
+      )}
+    </>
   );
 }
